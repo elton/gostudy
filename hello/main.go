50fcb0a64c332c24b387fa49229454e287284dab
Elton Zheng <elton.zheng.sh@gmail.com>
2019-07-02 18:31:29 +0800

    hello world

---
+++ hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, 世界！")
+}
+
+func add(a, b int) int {
+	return a + b
+}
---
+++ hello/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	a, b, c = 1, 2, 3
+	res := add(a, b)
+	if res != c {
+		t.Fail()
+	}
+}
